5ab9024ec127c0c3dc0650bbd3020742e38fa676
Pepe García <pepe@pepegar.com>
2021-02-12 15:16:04 +0100

    add some docs to multiset

--- modules/engine/src/main/scala/com/gsk/kg/engine/Multiset.scala
+++ modules/engine/src/main/scala/com/gsk/kg/engine/Multiset.scala
@@ -6,25 +6,39 @@ import cats.kernel.Semigroup
 import org.apache.spark.sql.SQLContext
 import cats.kernel.Monoid
 
+
+/**
+  * A [[Multiset]], as expressed in SparQL terms.
+  *
+  * @see See [[https://www.w3.org/2001/sw/DataAccess/rq23/rq24-algebra.html]]
+  * @param bindings the variables this multiset expose
+  * @param dataframe the underlying data that the multiset contains
+  */
 final case class Multiset(
   bindings: Set[VARIABLE],
   dataframe: DataFrame
 ) {
 
+  /**
+    * Join two multisets following SparQL semantics.  If two multisets
+    * share some bindings, it performs an _inner join_ between them.
+    * If they don't share any common binding, it performs a cross join
+    * instead.
+    *
+    * @param other
+    * @return
+    */
   def join(other: Multiset) = (this, other) match {
     case (a, b) if a.isEmpty => b
     case (a, b) if b.isEmpty => a
     case (Multiset(aBindings, aDF), Multiset(bBindings, bDF)) if aBindings.intersect(bBindings).isEmpty =>
       val df = aDF.as("a")
         .crossJoin(bDF.as("b"))
-
       Multiset(aBindings.union(bBindings), df)
-
     case (a, b) =>
       val common = a.bindings.intersect(b.bindings)
       val df = a.dataframe.as("a")
         .join(b.dataframe.as("b"), common.map(_.s).toSeq)
-
       Multiset(a.bindings.union(b.bindings), df)
   }
 
