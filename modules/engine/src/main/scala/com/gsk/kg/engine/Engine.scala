41ed15d47c8a8fbab7449a4f5342fce4bab0da08
Pepe García <pepe@pepegar.com>
2021-02-19 16:54:31 +0100

    extract extendF out of the algebra

--- modules/engine/src/main/scala/com/gsk/kg/engine/Engine.scala
+++ modules/engine/src/main/scala/com/gsk/kg/engine/Engine.scala
@@ -21,6 +21,7 @@ import com.gsk.kg.sparqlparser.Query
 import com.gsk.kg.sparqlparser.Query.Construct
 import com.gsk.kg.sparqlparser.StringFunc._
 import com.gsk.kg.sparqlparser.StringVal._
+import com.gsk.kg.sparqlparser.Expression
 
 object Engine {
 
@@ -32,43 +33,53 @@ object Engine {
     AlgebraM[M, ExprF, Multiset] {
       case BGPF(triples) => evaluateBGPF(triples)
       case TripleF(s, p, o) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("TripleF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("TripleF not implemented").asLeft[Multiset]
+        )
       case LeftJoinF(l, r) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("LeftJoinF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("LeftJoinF not implemented").asLeft[Multiset]
+        )
       case FilteredLeftJoinF(l, r, f) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("FilteredLeftJoinF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError
+            .General("FilteredLeftJoinF not implemented")
+            .asLeft[Multiset]
+        )
       case UnionF(l, r) =>
         l.union(r).pure[M]
       case ExtendF(bindTo, bindFrom, r) =>
-        val bf = ExpressionF.getVariable(bindFrom)
-        val separator = ExpressionF.getString(bindFrom)
-
-        val either = for {
-          columnName <- bf.toRight(EngineError.General("unable to find column in STRAFTER"))
-          sep <- separator.toRight(EngineError.General("unable to find separator in STRAFTER"))
-          column = r.dataframe(columnName)
-        } yield r.applyFunc(
-          bindTo,
-          column,
-          col => Func.strafter(col, sep)
-        )
-        StateT.liftF[Result, DataFrame, Multiset](either)
+        evaluateExtendF(bindTo, bindFrom, r)
       case FilterF(funcs, expr) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("FilterF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("FilterF not implemented").asLeft[Multiset]
+        )
       case JoinF(l, r) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("JoinF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("JoinF not implemented").asLeft[Multiset]
+        )
       case GraphF(g, e) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("GraphF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("GraphF not implemented").asLeft[Multiset]
+        )
       case DistinctF(r) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("DistinctF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("DistinctF not implemented").asLeft[Multiset]
+        )
       case OffsetLimitF(offset, limit, r) =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("OffsetLimitF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("OffsetLimitF not implemented").asLeft[Multiset]
+        )
       case OpNilF() =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("OpNilF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("OpNilF not implemented").asLeft[Multiset]
+        )
       case ProjectF(vars, r) =>
         r.select(vars: _*).pure[M]
       case TabUnitF() =>
-        StateT.liftF[Result, DataFrame, Multiset](EngineError.General("TabUnitF not implemented").asLeft[Multiset])
+        StateT.liftF[Result, DataFrame, Multiset](
+          EngineError.General("TabUnitF not implemented").asLeft[Multiset]
+        )
     }
 
   def evaluate(
@@ -84,6 +95,28 @@ object Engine {
       .runA(dataframe)
       .map(_.dataframe)
       .map(QueryExecutor.execute(query))
+  }
+
+  private def evaluateExtendF(
+      bindTo: VARIABLE,
+      bindFrom: Expression,
+      r: Multiset
+  ) = {
+    val bf = ExpressionF.getVariable(bindFrom)
+    val separator = ExpressionF.getString(bindFrom)
+    val either = for {
+      columnName <-
+        bf.toRight(EngineError.General("unable to find column in STRAFTER"))
+      sep <- separator.toRight(
+        EngineError.General("unable to find separator in STRAFTER")
+      )
+      column = r.dataframe(columnName)
+    } yield r.applyFunc(
+      bindTo,
+      column,
+      col => Func.strafter(col, sep)
+    )
+    StateT.liftF[Result, DataFrame, Multiset](either)
   }
 
   private def evaluateBGPF(
