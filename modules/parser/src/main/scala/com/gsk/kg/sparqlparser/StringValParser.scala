bebd7f55b8debad2f5bc76583f80b12c52db92f9
Pepe García <pepe@pepegar.com>
2021-02-22 18:57:51 +0100

    avoid quoting URIs

--- modules/parser/src/main/scala/com/gsk/kg/sparqlparser/StringValParser.scala
+++ modules/parser/src/main/scala/com/gsk/kg/sparqlparser/StringValParser.scala
@@ -8,7 +8,9 @@ object StringValParser {
   def string[_:P]:P[STRING] = P("\"" ~ CharsWhile(_ != '\"').! ~ "\"").map{STRING}
   //TODO improve regex. Include all valid sparql varnames in spec: https://www.w3.org/TR/sparql11-query/#rPN_CHARS_BASE
   def variable[_:P]:P[VARIABLE] = P("?" ~ CharsWhileIn("a-zA-Z0-9_")).!.map{VARIABLE}
-  def urival[_:P]:P[URIVAL] = P("<" ~ (CharsWhile(_ != '>')) ~ ">").!.map{URIVAL}
+  def urival[_:P]:P[URIVAL] = P("<" ~ (CharsWhile(_ != '>')) ~ ">").!.map{URIVAL}.map {
+    case URIVAL(str) => URIVAL(str.stripPrefix("<").stripSuffix(">"))
+  }
   def num[_:P]:P[NUM] = P(CharsWhileIn("0-9")).!.map{NUM}
   def optionLong[_:P]:P[Option[Long]] = P(CharsWhileIn("0-9_")).!.map{ s =>
     if (s.contains('_')) None else Some(s.toLong)
